4acea3290fade1cc1143e85fa5916a4acfcc9159
Prabhnoor Saluja <isaluja1811@gmail.com>
2020-08-10 17:06:05 +0530

    upload to GitHub

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:tips/ui/BillSplit.dart';
+
+
+void main() => runApp(new MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: BillSplit(),
+));
